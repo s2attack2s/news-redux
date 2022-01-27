37ddaec61e17ae2aace514d023bf9d7a00976e5b
DESKTOP-RBMQ7JR\MienTrungComputer <s2attack2s@gmail.com>
2022-01-27 09:42:48 +0700

    paginate

--- src/components/view/home/home.jsx
+++ src/components/view/home/home.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Route, Routes } from "react-router-dom";
 import Contact from "../contact/contact";
 import News from "../news/news";
 class Home extends Component {
--- src/components/view/news/news.jsx
+++ src/components/view/news/news.jsx
@@ -2,14 +2,63 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import "../../css/news.css";
 import ListNews from "./list-new";
+import {
+  AiFillStepBackward,
+  AiFillStepForward,
+  AiOutlineFastBackward,
+  AiOutlineFastForward,
+} from "react-icons/ai";
 
 class News extends Component {
   constructor(props) {
     super(props);
     this.state = {
       posts: this.props.posts,
+      currentPage: 1,
+      newsPerPage: 3,
     };
   }
+  componentDidMount() {
+    let { posts, newsPerPage } = this.state;
+    let lastpage = Math.ceil(posts.length / newsPerPage);
+    this.setState({
+      lastpage,
+    });
+  }
+  chosePage = (event) => {
+    this.setState({
+      currentPage: Number(event.target.id),
+    });
+  };
+  pre = () => {
+    let { currentPage } = this.state;
+    if (currentPage > 1) {
+      this.setState({
+        currentPage: --currentPage,
+      });
+    }
+  };
+  next = () => {
+    let { currentPage, lastpage } = this.state;
+    if (currentPage < lastpage) {
+      this.setState({
+        currentPage: ++currentPage,
+      });
+    }
+  };
+  first = () => {
+    this.setState({
+      currentPage: 1,
+    });
+  };
+  last = (e) => {
+    let { currentPage, lastpage } = this.state;
+    if (currentPage < lastpage) {
+      this.setState({
+        currentPage: lastpage,
+      });
+    }
+  };
   changeTitle = (e) => {
     let tle = e.target.value;
     let PostFilter = this.props.posts;
@@ -31,6 +80,18 @@ class News extends Component {
     }
   };
   render() {
+    let { posts, newsPerPage, currentPage, lastpage } = this.state;
+    const indexOfLastNews = currentPage * newsPerPage;
+    const indexOfFirstNews = indexOfLastNews - newsPerPage;
+    const currentPosts = posts.slice(indexOfFirstNews, indexOfLastNews);
+    const renderPosts = currentPosts.map((val, key) => {
+      return <ListNews key={key} value={val} id={key} />;
+    });
+    const pageNumbers = [];
+    for (let i = 1; i <= lastpage; i++) {
+      pageNumbers.push(i);
+    }
+
     return (
       <div className="news container">
         <div className="title-h3">
@@ -41,10 +102,56 @@ class News extends Component {
             onChange={this.changeTitle}
           />
         </div>
+        {renderPosts}
+        <ul id="page-numbers">
+          {currentPage > 1 ? (
+            <>
+              <li onClick={this.first} className=" btn btn-light">
+                <AiOutlineFastBackward></AiOutlineFastBackward>
+              </li>
+              <li onClick={this.pre} className=" btn btn-light">
+                <AiFillStepBackward></AiFillStepBackward>
+              </li>
+            </>
+          ) : (
+            ""
+          )}
 
-        {this.state.posts.map((val, key) => (
-          <ListNews key={key} value={val} id={key} />
-        ))}
+          {pageNumbers.map((number) => {
+            if (currentPage === number) {
+              return (
+                <li key={number} id={number} className="active btn btn-primary">
+                  {number}
+                </li>
+              );
+            } else {
+              return (
+                <li
+                  key={number}
+                  id={number}
+                  onClick={this.chosePage}
+                  className="btn btn-light"
+                >
+                  {number}
+                </li>
+              );
+            }
+          })}
+          {
+            (currentPage = lastpage ? (
+              <>
+                <li onClick={this.next} className=" btn btn-light">
+                  <AiFillStepForward></AiFillStepForward>
+                </li>
+                <li onClick={this.last} className=" btn btn-light">
+                  <AiOutlineFastForward></AiOutlineFastForward>
+                </li>
+              </>
+            ) : (
+              ""
+            ))
+          }
+        </ul>
       </div>
     );
   }
@@ -54,8 +161,5 @@ let mapStateToprops = (state) => {
     posts: state.post.posts,
   };
 };
-let mapDispatchToProps = (dispatch) => {
-  return {};
-};
 
-export default connect(mapStateToprops, mapDispatchToProps)(News);
+export default connect(mapStateToprops, null)(News);
