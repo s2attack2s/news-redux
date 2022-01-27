cbce8e5739a01778b381b12262b0c0f78b4a05a6
DESKTOP-RBMQ7JR\MienTrungComputer <s2attack2s@gmail.com>
2022-01-27 14:20:36 +0700

    create-db

---
+++ src/db/quiz.js
@@ -0,0 +1,162 @@
+{
+    "quiz": {
+        "sport": {
+            "q1": {
+                "question": "Which one is correct team name in NBA?",
+                "options": [
+                    "New York Bulls",
+                    "Los Angeles Kings",
+                    "Golden State Warriros",
+                    "Huston Rocket"
+                ],
+                "answer": "Huston Rocket"
+            }
+        },
+        "maths": {
+            "q1": {
+                "question": "5 + 7 = ?",
+                "options": [
+                    "10",
+                    "11",
+                    "12",
+                    "13"
+                ],
+                "answer": "12"
+            },
+            "q2": {
+                "question": "12 - 8 = ?",
+                "options": [
+                    "1",
+                    "2",
+                    "3",
+                    "4"
+                ],
+                "answer": "4"
+            }
+            "q3": {
+                "question": "3 + 7 = ?",
+                "options": [
+                    "10",
+                    "11",
+                    "12",
+                    "13"
+                ],
+                "answer": "10"
+            },
+            "q4": {
+                "question": "4 + 7 = ?",
+                "options": [
+                    "10",
+                    "11",
+                    "12",
+                    "13"
+                ],
+                "answer": "11"
+            },
+            "q5": {
+                "question": "2 + 27 = ?",
+                "options": [
+                    "10",
+                    "11",
+                    "29",
+                    "13"
+                ],
+                "answer": "29"
+            },
+            "q6": {
+                "question": "5 + 10 = ?",
+                "options": [
+                    "10",
+                    "11",
+                    "12",
+                    "15"
+                ],
+                "answer": "15"
+            },"q7": {
+                "question": "3 - 3 = ?",
+                "options": [
+                    "0",
+                    "11",
+                    "12",
+                    "13"
+                ],
+                "answer": "0"
+            },
+            "q8": {
+                "question": "15 + 7 = ?",
+                "options": [
+                    "10",
+                    "11",
+                    "32",
+                    "13"
+                ],
+                "answer": "32"
+            },"q9": {
+                "question": "52 + 7 = ?",
+                "options": [
+                    "10",
+                    "11",
+                    "12",
+                    "59"
+                ],
+                "answer": "59"
+            },
+            "q10": {
+                "question": "35 + 7 = ?",
+                "options": [
+                    "10",
+                    "11",
+                    "42",
+                    "13"
+                ],
+                "answer": "42"
+            },"q11": {
+                "question": "1 + 7 = ?",
+                "options": [
+                    "10",
+                    "8",
+                    "12",
+                    "13"
+                ],
+                "answer": "8"
+            },
+            "q12": {
+                "question": "3 + 3 = ?",
+                "options": [
+                    "10",
+                    "6",
+                    "12",
+                    "13"
+                ],
+                "answer": "6"
+            },"q13": {
+                "question": "51 + 7 = ?",
+                "options": [
+                    "10",
+                    "11",
+                    "12",
+                    "58"
+                ],
+                "answer": "58"
+            },"q14": {
+                "question": "50 + 7 = ?",
+                "options": [
+                    "10",
+                    "11",
+                    "57",
+                    "13"
+                ],
+                "answer": "57"
+            },"q1": {
+                "question": "53 + 7 = ?",
+                "options": [
+                    "10",
+                    "11",
+                    "12",
+                    "60"
+                ],
+                "answer": "60"
+            },
+        }
+    }
+}
